74e605b11051d00b18c5f2a05019e67e4f20f19f
christina makatura <cnmak@comcast.net>
2021-11-05 01:56:00 -0600

    Adding outline

--- src/index.js
+++ src/index.js
@@ -1,41 +1,27 @@
-  document.addEventListener("DOMContentLoaded", () => {
-    let form = document.querySelector('form')
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-//create a URL that goes to API of submitted item
-let link = ('https://api.dictionaryapi.dev/api/v2/entries/en/' + e.target.word_description.value)
-fetch(link)
-.then(res => res.json())
-.then (json => buildList(json))
+//DOM content loaded event
+//Create form with submit button and prevent default refresh
+//create link for fetch that will go to API of submitted word
+//fetch data, json-ify, pass json into function that will return definition
+//reset form
 
-      form.reset()
-    })  
-  });
-
-  //function that shows the searched word and the definition
-  function buildList(word){
-    
-    let p = document.createElement('p')
-    p.textContent = `${word[0].word}: ${word[0].meanings[0].definitions[0].definition}`
-    document.querySelector('#list').appendChild(p)
-
-    // Add button to clear definition
-    let btn = document.createElement('button')
-    btn.addEventListener('click', handleDelete)
-    btn.textContent = " Clear"
-    p.appendChild(btn)
-    document.querySelector('#list').appendChild(p)
-    };
-
-    //Deletes definition
-    function handleDelete(e){
-      e.target.parentNode.remove();
-    }
-
-
-
- 
+//create function that takes json data and navigates to definition
+//create a container for each definition with save and clear button
+//Save button appends definition to sidebar
+//clear button will delete definition 
+//append container to list of definitions
   
+  
+  
+ 
+
+
+
+
+
+
+
+
+
 
 
   
@@ -45,9 +31,4 @@ fetch(link)
 
 
 
-
-// Tried to create an h3 word and p definition but couldnt figure out how to delete both with one button
-//let h3 = document.createElement('h3')
-    //h3.textContent = word[0].word
-      //h3.textContent = `${word[0].word} \n ${word[0].meanings[0].definitions[0].definition}`
-      //document.querySelector('#list').appendChild(h3)
+  
