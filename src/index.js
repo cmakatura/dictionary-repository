2d5150e983e6f866950bf996f8d819a90d5abce9
christina makatura <cnmak@comcast.net>
2021-11-05 02:27:19 -0600

    editing indentation

--- src/index.js
+++ src/index.js
@@ -17,47 +17,47 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-  //create function that takes json data and navigates to definition and posts info to content
-  function buildList(word){
+//create function that takes json data and navigates to definition and posts info to content
+function buildList(word){
  
-    //create a container for each definition with save and clear button
-    let definition = document.createElement('li')
-    definition.className = "word-container"
-    definition.innerHTML = `
-      <div class = "content">
-        <h4>${word[0].word}</h4>
-        <p>${word[0].meanings[0].definitions[0].definition}</p>
-      </div>
-      <div>
-        <button class="btn" id = "clear">Clear</button>
-        <button class="btn" id = "save">Save</button>
-      </div>
-    `
-    definition.querySelector('#save').addEventListener('click', () => {
+  //create a container for each definition with save and clear button
+  let definition = document.createElement('li')
+  definition.className = "word-container"
+  definition.innerHTML = `
+    <div class = "content">
+      <h4>${word[0].word}</h4>
+      <p>${word[0].meanings[0].definitions[0].definition}</p>
+    </div>
+    <div>
+      <button class="btn" id = "clear">Clear</button>
+      <button class="btn" id = "save">Save</button>
+    </div>
+  `
+  definition.querySelector('#save').addEventListener('click', () => {
     
-    let savedSearch = document.createElement('li')
-    savedSearch.className = "word-container"
-    savedSearch.innerHTML = `
-      <div class = "saved-content">
-        <p>${word[0].word}: ${word[0].meanings[0].definitions[0].definition}</p>
+  let savedSearch = document.createElement('li')
+  savedSearch.className = "word-container"
+  savedSearch.innerHTML = `
+    <div class = "saved-content">
+      <p>${word[0].word}: ${word[0].meanings[0].definitions[0].definition}</p>
       </div>
-      `
-    //Save button appends definition to sidebar
-    document.querySelector('#sidebar').appendChild(savedSearch)
+  `
+  //Save button appends definition to sidebar
+  document.querySelector('#sidebar').appendChild(savedSearch)
     
-    })
+  })
 
 
-    //clear button will delete definition
-    definition.querySelector('#clear').addEventListener('click', () => {
-      definition.remove();
-    })
+  //clear button will delete definition
+  definition.querySelector('#clear').addEventListener('click', () => {
+    definition.remove();
+  })
 
-    //append container to list of definitions
-    document.querySelector('#list').appendChild(definition)
+  //append container to list of definitions
+  document.querySelector('#list').appendChild(definition)
 
 
-    };
+};
     
   
   
