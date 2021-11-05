0262e54e1a0bd95c34c8a5b01d00e960c4943f77
christina makatura <cnmak@comcast.net>
2021-11-05 02:02:33 -0600

    adding function to build definition list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,60 @@
 //DOM content loaded event
-//Create form with submit button and prevent default refresh
-//create link for fetch that will go to API of submitted word
-//fetch data, json-ify, pass json into function that will return definition
-//reset form
+document.addEventListener("DOMContentLoaded", () => {
+  //Create form with submit button and prevent default refresh
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+  //create link for fetch that will go to API of submitted word
+  let link = ('https://api.dictionaryapi.dev/api/v2/entries/en/' + e.target.word_description.value)
+  //fetch data, json-ify, pass json into function that will return definition
+  fetch(link)
+    .then(response => response.json())
+    .then (json => buildList(json))
+    //reset form
+    form.reset()
+  })  
+});
 
-//create function that takes json data and navigates to definition
-//create a container for each definition with save and clear button
-//Save button appends definition to sidebar
-//clear button will delete definition 
-//append container to list of definitions
+
+  //create function that takes json data and navigates to definition and posts info to content
+  function buildList(word){
+ 
+    //create a container for each definition with save and clear button
+    let definition = document.createElement('li')
+    definition.className = "word-container"
+    definition.innerHTML = `
+    <div class = "content">
+      <h4>${word[0].word}</h4>
+      <p>${word[0].meanings[0].definitions[0].definition}</p>
+      </div>
+      <div>
+      <button class="btn" id = "clear">Clear</button>
+      <button class="btn" id = "save">Save</button>
+    </div>
+    `
+    definition.querySelector('#save').addEventListener('click', () => {
+    
+      let savedSearch = document.createElement('li')
+    savedSearch.className = "word-container"
+    savedSearch.innerHTML = `
+    <div class = "saved-content">
+      <p>${word[0].word}: ${word[0].meanings[0].definitions[0].definition}</p>
+      </div>
+      `
+      //Save button appends definition to sidebar
+      document.querySelector('#sidebar').appendChild(savedSearch)
+    
+    })
+    //clear button will delete definition
+    definition.querySelector('#clear').addEventListener('click', () => {
+      definition.remove()
+    })
+
+    //append container to list of definitions
+    document.querySelector('#list').appendChild(definition)
+    };
+    
   
   
   
